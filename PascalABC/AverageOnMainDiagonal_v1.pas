4b159b69e7c65686b5626a867b6e678783f8c0e8
denisjurbi4 <denminecraft8@gmail.com>
2025-12-16 20:29:33 +0200

    Add files via upload

---
+++ PascalABC/AverageOnMainDiagonal_v1.pas
@@ -0,0 +1,37 @@
+﻿program AverageMainDiagonal;
+
+uses crt;
+
+const
+  N = 4;  { Розмір квадратного масиву }
+
+var
+  A: array[1..N, 1..N] of Integer;
+  i: Integer;
+  sum: Real;
+  average: Real;
+
+begin
+  clrscr;
+  
+  { Введення елементів масиву }
+  writeln('Введіть елементи ', N, 'x', N, ' масиву:');
+  for i := 1 to N do
+    for var j := 1 to N do
+    begin
+      write('A[', i, ',', j, '] = ');
+      readln(A[i, j]);
+    end;
+
+  { Обчислення суми елементів головної діагоналі }
+  sum := 0;
+  for i := 1 to N do
+    sum := sum + A[i, i];
+
+  { Обчислення середнього арифметичного }
+  average := sum / N;
+
+  writeln('Середнє арифметичне елементів головної діагоналі: ', average:0:2);
+
+  readln;
+end.
